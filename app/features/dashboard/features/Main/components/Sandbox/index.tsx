1f8807257c22a953c8e9acce7be1d10516b8fd09
yaredtsy <tsyared@gmail.com>
2026-01-27 18:30:46 +0300

    redeploy

--- app/features/dashboard/features/Main/components/Sandbox/index.tsx
+++ app/features/dashboard/features/Main/components/Sandbox/index.tsx
@@ -9,7 +9,6 @@ import { useEffect, useRef, useState } from "react";
 
 /**
  * Sandbox Component.
- *
  * Orchestrates multiple features (Playground, Test, Logs) in a tabbed interface.
  */
 export default function Sandbox({ tabId }: { tabId: string }) {
